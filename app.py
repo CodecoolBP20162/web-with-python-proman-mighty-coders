dc31411dae7598d3319feea6bfb02be8ef679c19
anarch1986 <varga.tamas19860808@gmail.com>
2017-03-20 14:41:08 +0100

    LocalStorage testing started

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+
+@app.route('/')
+def show_user_stories():
+    return render_template('test.html')
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=5000, debug=True)
--- static/script.js
+++ static/script.js
@@ -1,3 +1,8 @@
 /**
  * Created by okocsis90 on 2017.03.20..
  */
+$(document).ready(function() {
+    $("button").click(function() {
+        $("p").slideToggle();
+    });
+});

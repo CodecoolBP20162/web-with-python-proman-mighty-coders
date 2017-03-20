6df2d8588ef130af46fceaa653e591d0862079e6
okocsis90 <o.kocsis90@gmail.com>
2017-03-20 14:25:41 +0100

    basic board added

--- static/script.js
+++ static/script.js
@@ -1,3 +1,13 @@
 /**
  * Created by okocsis90 on 2017.03.20..
  */
+$(document).ready(function() {
+    $('.card-overlay').hover(
+        function() {
+            $(this).addClass('active')
+        },
+        function() {
+            $(this).removeClass('active')
+        }
+    );
+});

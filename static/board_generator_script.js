924f81cf815fc0490f9da84253963d6e5c4b242d
anarch1986 <varga.tamas19860808@gmail.com>
2017-03-21 16:25:51 +0100

    dynamic id for boards done

--- static/board_generator_script.js
+++ static/board_generator_script.js
@@ -2,9 +2,9 @@
  * Created by okocsis90 on 2017.03.20..
  */
 $(document).ready(function() {
-    var boardTemplate = '<div class="col-sm-4"><div class="board"><h3 class="board_title">Project title</h3></div></div>';
-    if (localStorage["board"]) {
-        var jsonBoard = localStorage["board"];
+    var boardTemplate = '<div class="col-sm-4" id="board0"><div class="board"><h3 class="board_title">Project title</h3></div></div>';
+    if (localStorage["boards"]) {
+        var jsonBoard = localStorage["boards"];
         var importBoard = JSON.parse(jsonBoard);
         var newBoard = boardTemplate;
         newBoard.children().find('.board_title').val(importBoard.title);
@@ -31,13 +31,12 @@ $(document).ready(function() {
 
     $(".circle").click(function() {
         if ($(".board")[0]) {
-            // var num = parseInt($(".col-sm-4:last").attr("id").match(/(\d+)$/)[0], 10) + 1;
-            // var newBoard = $(".col-sm-4:last").clone(true, true).prop("id", "board" + num);
-            // newBoard.children().find('.board_title').val("");
+            var num = parseInt($(".col-sm-4:last").attr("id").match(/(\d+)$/)[0], 10) + 1;
+            var newBoard = $(boardTemplate).prop("id", "board" + num);
             $("#board_row").append(newBoard);
         } else {
             $("#no_boards").remove();
-            newBoard = boardTemplate;
+            var newBoard = $(boardTemplate).prop("id", "board" + 1);
             $("#board_row").append(newBoard);
         }
         alert("Created!");

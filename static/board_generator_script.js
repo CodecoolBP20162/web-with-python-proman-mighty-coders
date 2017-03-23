835c88502699625630123e75227f0564d99a2ef3
okocsis90 <o.kocsis90@gmail.com>
2017-03-23 13:27:44 +0100

    proxy pattern implemented with load as well

--- static/board_generator_script.js
+++ static/board_generator_script.js
@@ -3,25 +3,45 @@
  */
 
 var boardTemplate = '<div class="col-sm-4" id="board0"><div class="board"><h3 class="board_title" id ="title">Project title</h3></div></div>';
+var proxyObject = new Proxy(handlingLocalStorage);
+
 
 function handlingLocalStorage() {
-    this.save = function(num) {
+    this.save = function (num) {
         var boardObject = {
-        board_id: document.getElementById("board" + num).id,
-        title_id: document.getElementById("title" + num).id,
-        title: document.getElementById("title" + num).innerHTML
+            board_id: document.getElementById("board" + num).id,
+            title_id: document.getElementById("title" + num).id,
+            title: document.getElementById("title" + num).innerHTML
+        };
+        var jsonBoard = JSON.stringify(boardObject);
+        localStorage.setItem(document.getElementById("board" + num).id, jsonBoard);
     };
-    var jsonBoard = JSON.stringify(boardObject);
-    localStorage.setItem(document.getElementById("board" + num).id, jsonBoard);
 
+    this.load = function () {
+        if (localStorage.length > 0) {
+            for (var i = 0; i < localStorage.length; i++) {
+                if (localStorage.key(i).includes("board")) {
+                    var importBoard = localStorage.getItem(localStorage.key(i));
+                    var jsonBoard = JSON.parse(importBoard);
+                    var newBoard = $(boardTemplate).prop("id", jsonBoard.board_id);
+                    newBoard.children().find('#title').prop("id", jsonBoard.title_id);
+                    $("#board_row").append(newBoard);
+                    document.getElementById(jsonBoard.title_id).innerHTML = jsonBoard.title;
+                }
+            }
+        } else {
+            $("#board_row").append('<div class="col-sm-12" id="no_boards">There are no boards in the system. Start working NOW!</div>');
+        }
     };
 }
 
 function Proxy(currentObject) {
     this.imp = new currentObject();
-
     this.proxySave = function(num) {
         this.imp.save(num)
+    };
+    this.proxyLoad = function() {
+        this.imp.load()
     };
 }
 
@@ -40,25 +60,11 @@ var create = function(title) {
     newBoard.children().find('#title').prop("id", "title" + num);
     $("#board_row").append(newBoard);
     document.getElementById("title" + num).innerHTML = title;
-    var proxyObject = new Proxy(handlingLocalStorage);
     proxyObject.proxySave(num);
 };
 
 var display = function() {
-    if (localStorage.length > 0) {
-        for (var i = 0; i < localStorage.length; i++) {
-            if (localStorage.key(i).includes("board")) {
-                var importBoard = localStorage.getItem(localStorage.key(i));
-                var jsonBoard = JSON.parse(importBoard);
-                var newBoard = $(boardTemplate).prop("id", jsonBoard.board_id);
-                newBoard.children().find('#title').prop("id", jsonBoard.title_id);
-                $("#board_row").append(newBoard);
-                document.getElementById(jsonBoard.title_id).innerHTML = jsonBoard.title;
-            }
-        }
-    } else {
-        $("#board_row").append('<div class="col-sm-12" id="no_boards">There are no boards in the system. Start working NOW!</div>');
-    }
+    proxyObject.proxyLoad();
 };
 
 $(document).ready(function() {

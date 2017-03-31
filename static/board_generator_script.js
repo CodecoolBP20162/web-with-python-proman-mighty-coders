77e96b529349b3c4190f355a2a2055a192a45f29
anarch1986 <varga.tamas19860808@gmail.com>
2017-03-31 12:50:55 +0200

    newly created boards now have cards attribute

--- static/board_generator_script.js
+++ static/board_generator_script.js
@@ -2,7 +2,7 @@
  * Created by okocsis90 on 2017.03.20..
  */
 
-var boardTemplate = '<div class="col-sm-3" id="board0"><div class="board"><h3 class="board_title" id ="title">Project title</h3></div></div>';
+var boardTemplate = '<div class="col-sm-3" id="board0" data-cards="null"><div class="board"><h3 class="board_title" id ="title">Project title</h3></div></div>';
 var proxyObject = new dataLayer(handlingLocalStorage);
 
 
@@ -11,7 +11,8 @@ function handlingLocalStorage() {
         var boardObject = {
             board_id: document.getElementById("board" + num).id,
             title_id: document.getElementById("title" + num).id,
-            title: document.getElementById("title" + num).innerHTML
+            title: document.getElementById("title" + num).innerHTML,
+            cards: $("#board" + num).attr("data-cards")
         };
         var jsonBoard = JSON.stringify(boardObject);
         localStorage.setItem(document.getElementById("board" + num).id, jsonBoard);
@@ -24,6 +25,7 @@ function handlingLocalStorage() {
                     var importBoard = localStorage.getItem(localStorage.key(i));
                     var jsonBoard = JSON.parse(importBoard);
                     var newBoard = $(boardTemplate).prop("id", jsonBoard.board_id);
+                    newBoard.attr("data-cards", jsonBoard.cards);
                     newBoard.children().find('#title').prop("id", jsonBoard.title_id);
                     $("#board_row").append(newBoard);
                     document.getElementById(jsonBoard.title_id).innerHTML = jsonBoard.title;
@@ -58,6 +60,8 @@ var create = function(title) {
     }
     var newBoard = $(boardTemplate).prop("id", "board" + num);
     newBoard.children().find('#title').prop("id", "title" + num);
+    card_list = []
+    newBoard.attr("data-cards", JSON.stringify(card_list));
     $("#board_row").append(newBoard);
     document.getElementById("title" + num).innerHTML = title;
     proxyObject.save(num);

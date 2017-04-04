1b5330c44f0a35af5f97aea2fa282fa0553b13f7
anarch1986 <varga.tamas19860808@gmail.com>
2017-04-04 07:28:58 +0200

    card icons now placed in the right side of card

--- static/card_generator_script.js
+++ static/card_generator_script.js
@@ -1,5 +1,5 @@
 var cardTemplate = '<li class="card" id="card0" data-parent-board="parent_board" data-status="new" data-order="none">Card title</li>';
-var buttons = '<span class="placeholder">     </span><div class="edit-delete-wrapper" id="card-icons"><span class="glyphicon glyphicon-trash" id="delete_card" title="Delete cards"></span><span class="glyphicon glyphicon-pencil" id="edit_card" title="Edit cards"></span></div>'
+var buttons = '<div class="edit-delete-wrapper" id="card-icons"><span class="glyphicon glyphicon-trash" id="delete_card" title="Delete cards"></span><span class="glyphicon glyphicon-pencil" id="edit_card" title="Edit cards"></span></div>'
 var dataLayerObj = new dataLayer(handlingLocalStorage);
 
 
@@ -50,7 +50,7 @@ var create = function(title) {
     newCard.attr("data-order", order);
     var parentBoard = getID();
     newCard.attr("data-parent-board", parentBoard);
-    newCard.html(buttons + title);
+    newCard.html(title + buttons);
     $("#new").append(newCard);
     $(".status_list").sortable("refresh");
     dataLayerObj.saveCard($("#card" + num));

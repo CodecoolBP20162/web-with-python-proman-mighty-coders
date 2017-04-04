fbf4975dd34fdb2608890d9fb9e3c9448f33465c
Agnes Bosskay <dynuchu@gmail.com>
2017-04-04 14:09:35 +0200

    os path fixed

--- app.py
+++ app.py
@@ -1,17 +1,33 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g
 from database_layer.saveToDatabase import SaveToDatabase
+from database_layer.models import *
+
 
 app = Flask(__name__)
 
+
+@app.before_request
+def before_request():
+    """Connect to the database before each request."""
+    g.db = Database().db
+    g.db.connect()
+
+@app.after_request
+def after_request(response):
+    """Close the database connection after each request."""
+    g.db.close()
+    return response
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         req_form_dict = dict(request.form)
-        SaveToDatabase().fillRow(req_form_dict['board_id'][0],
+        db = SaveToDatabase()
+        db.fillRow(req_form_dict['board_id'][0],
                                  req_form_dict['title_id'][0],
                                  req_form_dict['title'][0])
     return render_template('boards.html')
+
 
 
 @app.route("/details/<board>")
